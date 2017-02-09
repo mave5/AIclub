8272f7c38b5351d5d2ff70b7e4c9142a46af34ed
mra <mra446@gmail.com>
2017-02-09 14:54:53 -0800

    Create ellipsefit.py

---
+++ tutorial/ellipsefit.py
@@ -0,0 +1,49 @@
+
+# mask: a 2D binary mask
+plt.imshow(mask,cmap='gray')
+
+# fit an ellipse to mask
+ellipse=get_ellipse(mask)
+(c,r),(b,a),alfa=ellipse # (c,r) center coordinate, (b,a) major and minor radius, alfa: rotation angle
+
+## create a binary mask from the fitted ellipse
+elipse_mask=np.zeros_like(mask)
+cv2.ellipse(elipse_mask, ellipse, (255,0, 0), -1) # elipse mask 
+
+
+
+## split ellipse into two circles or ellipses
+# centers of two circles/ellipses
+c1=(c-a/4*np.sin(alfa))
+r1=(r+b/4*np.cos(alfa))
+c2=(c+a/4*np.sin(alfa))
+r2=(r-b/4*np.cos(alfa))
+
+# coodiates of ellipses
+ellipse1=((c1,r1),(b/2,a/2),alfa)
+ellipse2=((c2,r2),(b/2,a/2),alfa)
+
+# coordinates of circles
+radius=int(a/4)
+c1=int(c1)
+c2=int(c2)
+r1=int(r1)
+r2=int(r2)
+
+## split to two circles
+two_circle_mask=np.zeros_like(mask)
+cv2.circle(two_circle_mask,(c1,r1), radius, (255,0,255), -1)
+cv2.circle(two_circle_mask,(c2,r2), radius, (255,0,255), -1)
+
+## split to two ellipses
+two_ellipse_mask=np.zeros_like(mask)
+cv2.ellipse(two_ellipse_mask, ellipse1, (255,0, 0), -1)
+v2.ellipse(two_ellipse_mask, ellipse2, (255,0, 255), -1)
+
+# plots
+plt.subplot(132)
+plt.imshow(two_circle_mask)
+
+plt.subplot(133)
+plt.imshow(two_ellipse_mask)
+
