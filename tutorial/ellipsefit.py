e13c1fa8a5094ecd0351137a142bd2ed2f8f212b
mra <mra446@gmail.com>
2017-02-10 11:38:02 -0800

    Update ellipsefit.py

--- tutorial/ellipsefit.py
+++ tutorial/ellipsefit.py
@@ -1,6 +1,6 @@
+n1=np.random.randint(Y_train.shape[0])
+mask=Y_train[n1,1,:]
 
-# mask: a 2D binary mask
-plt.imshow(mask,cmap='gray')
 
 # fit an ellipse to mask
 ellipse=get_ellipse(mask)
@@ -14,10 +14,19 @@ cv2.ellipse(elipse_mask, ellipse, (255,0, 0), -1) # elipse mask
 
 ## split ellipse into two circles or ellipses
 # centers of two circles/ellipses
-c1=(c-a/4*np.sin(alfa))
-r1=(r+b/4*np.cos(alfa))
-c2=(c+a/4*np.sin(alfa))
-r2=(r-b/4*np.cos(alfa))
+alfa=alfa-90.
+# convert to radians
+alfa=alfa*np.pi/180.
+if alfa<=(np.pi/2):
+    c1=(c-a/4.*np.cos(alfa))
+    r1=(r-a/4.*np.sin(alfa))
+    c2=(c+a/4.*np.cos(alfa))
+    r2=(r+a/4.*np.sin(alfa))
+else:
+    c1=(c+a/4.*np.cos(alfa))
+    r1=(r-a/4.*np.sin(alfa))
+    c2=(c-a/4.*np.cos(alfa))
+    r2=(r+a/4.*np.sin(alfa))
 
 # coodiates of ellipses
 ellipse1=((c1,r1),(b/2,a/2),alfa)
@@ -34,32 +43,26 @@ r2=int(r2)
 two_circle_mask=np.zeros_like(mask)
 cv2.circle(two_circle_mask,(c1,r1), radius, (255,0,255), -1)
 cv2.circle(two_circle_mask,(c2,r2), radius, (255,0,255), -1)
+cv2.ellipse(two_circle_mask, ellipse, (255,0, 255), 1)
 
 ## split to two ellipses
 two_ellipse_mask=np.zeros_like(mask)
 cv2.ellipse(two_ellipse_mask, ellipse1, (255,0, 0), -1)
-v2.ellipse(two_ellipse_mask, ellipse2, (255,0, 255), -1)
+cv2.ellipse(two_ellipse_mask, ellipse2, (255,0, 255), -1)
 
 # plots
-plt.subplot(132)
+plt.figure(figsize=(10,5))
+plt.subplot(141)
+plt.imshow(mask,cmap='gray')
+
+# mask: a 2D binary mask
+plt.subplot(142)
+#cv2.ellipse(mask, ellipse, (255,0, 255), -1)
+plt.imshow(elipse_mask)
+
+plt.subplot(143)
 plt.imshow(two_circle_mask)
 
-plt.subplot(133)
+plt.subplot(144)
 plt.imshow(two_ellipse_mask)
-
-
-## Passing functions with multiple return values as arguments in Python
-
-# function definition
-def func1(x1,y1):
-  #your code
-  return r1,t1
-
-# function definition
-def func2(x2,y2):
-  #your code
-  return r2,rt
-
-# main code
-func2(*func1(x1,y1))
-
+plt.show()
